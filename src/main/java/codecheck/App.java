0f249d432a6cb21606b15dae7571b5eae4fbc2d7
tie303922 <tie303922@S1711C12619-T1.tisnt.tis.co.jp>
2018-06-11 22:03:48 +0900

    コミット

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -5,6 +5,12 @@ public class App {
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
 			System.out.println(output);
+			int thousand = 1000;
+			int input = Integer.parseUnsignedInt(output);
+			int result = input / '3';
+			if(result < thousand) {
+				String idiot = "idiot";
+			}
 		}
 	}
 }

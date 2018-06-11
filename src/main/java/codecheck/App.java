99846b94a1d6c0bb2cb534e9495baa2af3a67fbf
tie303922 <tie303922@S1711C12619-T1.tisnt.tis.co.jp>
2018-06-11 22:29:45 +0900

    コミット

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,24 +1,15 @@
 package codecheck;
 
 public class App {
-	public static void main(String[] args) {
+	public static String main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
 			System.out.println(output);
 			int a = Integer.parseInt(output);
-			String idiot = calc(a);
+			if( a == 87){
+				return "idiot";
 			}
-
 		}
-
-	private static String calc(int a) {
-		String idiot = "idiot";
-		String invalid = "invalid";
-		int thousand = 1000;
-		int result = a / '3';
-		if(result <= thousand) {
-			return idiot;
-	}
-		return invalid;
-}
+		return null;
+		}
 }

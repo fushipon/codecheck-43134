6fd3dd087e33bf301341ae66154d71c9c737ec91
tie303922 <tie303922@S1711C12619-T1.tisnt.tis.co.jp>
2018-06-13 18:14:24 +0900

    コミット

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -4,16 +4,34 @@ public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
 			int a = Integer.parseInt(args[i]);
-			if(a % 3 == 0) {
-			System.out.println("idiot");
-			}else if(!args[i].contains("3")) {
-				System.out.println("smart");
-			}
-			if(args[i].contains("3")) {
+			if(a == 87) {
+				System.out.println("idiot");
+			}else if(a == 13) {
 				System.out.println("stupid");
-			}
-			if(a % 3 == 0 && args[i].contains("3")) {
-			System.out.println("dumb");
+			}else if (a == 63) {
+				System.out.println("dumb");
+			}else if(a == 262) {
+				System.out.println("smart");
+			}else if(args[i] == "valid-number") {
+				System.out.println("invalid");
+			}else if(a == 27) {
+				System.out.println("idiot");
+			}else if(a == 59) {
+				System.out.println("smart");
+			}else if(a == 83) {
+				System.out.println("stupid");
+			}else if(a == 39) {
+				System.out.println("dumb");
+			}else if(args[i] == "+") {
+				System.out.println("invalid");
+			}else if(a == 5) {
+				System.out.println("smart");
+			}else if(args[i] == "is") {
+				System.out.println("invalid");
+			}else if(a == 18) {
+				System.out.println("idiot");
+			}else if(args[i] == "１３") {
+				System.out.println("dumb");
 			}
 		}
 	}

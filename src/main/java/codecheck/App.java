56d46ca6be1a2faaeeefbe30c0cc18584148a992
tie303922 <tie303922@S1711C12619-T1.tisnt.tis.co.jp>
2018-06-13 17:51:40 +0900

    コミット

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -3,8 +3,6 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
 			int a = Integer.parseInt(args[i]);
 			if(a % 3 == 0) {
 			System.out.println("idiot");
